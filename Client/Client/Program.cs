3dd4ef714e37ec70a943707dee1426e77cdbcd80
phatdinhvn <phatdinhvn@gmail.com>
2018-03-09 14:33:47 +0700

    Ket noi Client-Server bang Socket va gui-nhan tin nhan

---
+++ Client/Client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+                IPEndPoint ip = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6789);
+                EndPoint ep = ip;
+                string s = "Hello server!";
+                byte[] gui = Encoding.ASCII.GetBytes(s);
+                client.SendTo(gui, ep);
+
+                byte[] nhan = new byte[1024];
+
+                client.ReceiveFrom(nhan, ref ep);
+
+                string r = Encoding.ASCII.GetString(nhan);
+
+                Console.WriteLine("Server: {0}", r);
+
+                client.Close();
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Server khong hop le.");
+            }
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            IPEndPoint ip = new IPEndPoint(IPAddress.Any,6789);
+            server.Bind(ip);
+
+            byte[] nhan = new byte[1024];
+            EndPoint ep = ip;
+            server.ReceiveFrom(nhan,ref ep);
+            string r = Encoding.ASCII.GetString(nhan);
+            Console.WriteLine("Client: {0}", r);
+
+            string s = "Hello Client!";
+            byte[] gui = Encoding.ASCII.GetBytes(s);
+            server.SendTo(gui, ep);
+
+            server.Close();
+        }
+    }
+}
